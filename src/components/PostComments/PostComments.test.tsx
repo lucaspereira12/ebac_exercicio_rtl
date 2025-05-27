5804a1b2b0925ff15061bb7cfbdcdc367226a1b2
lucas <lucas@teste.com>
2025-05-27 15:40:01 -0300

    fim do exercício

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,34 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve ser inserido dois comentários', () => {
+        render(<PostComment/>);
+
+        // Inserção do primeiro comentário
+
+        fireEvent.change(screen.getByTestId('comment-text'), {
+            target: {
+                value: 'Primeiro comentário adicionado através de teste'
+            }
+        });
+
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        // ----------------------------------------------------------------------
+
+        // Inserção do segundo comentário
+
+        fireEvent.change(screen.getByTestId('comment-text'), {
+            target: {
+                value: 'Segundo comentário adicionado através de teste'
+            }
+        });
+
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        // ----------------------------------------------------------------------
+
+        expect(screen.getAllByTestId('comment')).toHaveLength(2);
+    });
 });
